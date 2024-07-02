eeaa333c571eb9ea501d858a693af6e3c02ba853
beltracr <cristiabeltran0628@gamil.com>
2024-07-02 16:02:10 -0500

    actualizacion

--- proyecto1/frontend/frontendProyecto/src/app/pages/ingreso/ingreso.component.ts
+++ proyecto1/frontend/frontendProyecto/src/app/pages/ingreso/ingreso.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavegacionComponent } from '../../components/navegacion/navegacion.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ingreso',
   standalone: true,
-  imports: [NavegacionComponent, FooterComponent],
+  imports: [NavegacionComponent, FooterComponent, RouterLink],
   templateUrl: './ingreso.component.html',
   styleUrl: './ingreso.component.css'
 })
